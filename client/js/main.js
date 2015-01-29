b7a7bce2d32144c35925ef421ac300702e4efe6b
David Manthey <david.manthey@kitware.com>
2015-01-29 15:52:07 -0500

    Plot the first 10000 items.

--- client/js/main.js
+++ client/js/main.js
@@ -6,7 +6,6 @@ geoapp.App = geoapp.View.extend({
         geoapp.events.on('ga:navigateTo', this.navigateTo, this);
     },
     render: function () {
-        console.log('HERE');
         this.$el.html(geoapp.templates.controls());
         return this;
     },
@@ -66,6 +65,24 @@ function showMap(data) {
         geo_layer = geo_map.createLayer('feature');
         geo_feature = geo_layer.createFeature('point', {selectionAPI:true})
     }
+    geo_feature.data(data)
+        .style({
+//            fillColor: '#91bfff',
+            fillColor: 'black',
+            fillOpacity: 0.05,
+//            fillOpacity: 0.65,
+//            strokeColor: 'black',
+//            strokeWidth: 1,
+            strokeWidth: 0,
+            strokeOpacity: 0,
+            radius: 5,
+        })
+        .position(function (d) {
+            return {
+                x: d.pickup_longitude,
+                y: d.pickup_latitude
+            };
+        });
     //DWM::
     geo_map.draw();
 }
@@ -91,6 +108,7 @@ function triggerDraw(fromTimer) {
 
 
 $(document).ready(function () {
+    girder.apiRoot = 'api/v1';
     app = new geoapp.views.ControlsView({el: '#app-container', parentView: null});
 
 });
