0d6a3e85a62f0797714775c5df481cf089ed2a75
David Manthey <david.manthey@kitware.com>
2015-02-16 08:36:15 -0500

    Added the ability to reingest data in a random order.  Added options to only index a database after ingest rather than during it.

--- utils/load_taxi.py
+++ utils/load_taxi.py
@@ -19,16 +19,19 @@
 
 import csv
 import datetime
+import json
 import md5
+import mmap
 import pymongo
 import random
 import os
 import sys
+import tempfile
 import time
 import zipfile
 
 
-CollectionName = 'taxi'
+DBName = 'taxi'
 
 
 TypeTable = {
@@ -106,32 +109,140 @@ RevTable = {v: k for k, v in KeyTable.items()}
 CRevTable = {v: k for k, v in CKeyTable.items()}
 
 
+def copyDB(srcDB, destDB, opts={}):
+    """
+    Copy one database's trips table to another, randomizing the order of the
+    rows.
+
+    :param srcDB: the name of the source database.
+    :param destDB: the name of the destination database.
+    :param opts: if 'random' is present and truthy, ensure that the new
+                 base contains an appropriately named 'random' field that is in
+                 the same order as the destination rows.  If random is missing
+                 or falsy, ensure that such a field has been stripped from the
+                 rows.  The opts dictionary also affects index creation on the
+                 destination database.
+    """
+    global DBName
+    DBName = destDB
+
+    randKey = getKey('random')
+    starttime = time.time()
+    client = getDbConnection(srcDB)
+    database = client.get_default_database()
+    srcColl = database['trips']
+    rows = srcColl.find(spec={}, sort=[('_id', 1)], slave_okay=True,
+                        complie_re=False, manipulate=False, timeout=False)
+    numRows = rows.count()
+    numFiles = int(numRows / 1024 / 1024 / 2) + 1
+    files = []
+    for f in xrange(numFiles):
+        (fd, filename) = tempfile.mkstemp()
+        os.close(fd)
+        fptr = open(filename, 'w+b')
+        files.append({'name': filename, 'fptr': fptr, 'starts': [0]})
+    processed = 0
+    for row in rows:
+        if '_id' in row:
+            del row['_id']
+        if randKey in row:
+            del row[randKey]
+        fnum = random.randint(0, numFiles-1)
+        files[fnum]['fptr'].write(json.dumps(row)+',\n')
+        files[fnum]['starts'].append(files[fnum]['fptr'].tell())
+        processed += 1
+        if not processed % 10000:
+            elapsed = time.time() - starttime
+            if elapsed:
+                left = (numRows - processed) * (elapsed / processed)
+            else:
+                left = 0
+            sys.stdout.write('%d/%d %3.1fs  %3.1fs left  \r' % (
+                processed, numRows, elapsed, left))
+            sys.stdout.flush()
+        if False and processed == 250000:
+            numRows = processed
+            break
+    for f in files:
+        f['fptr'].close()
+                    
+    if sys.platform == 'win32':
+        os.system('net stop MongoDB >NUL 2>NUL')
+        os.system('net start MongoDB >NUL 2>NUL')
+
+    starttime = time.time()
+    (fd, filename) = tempfile.mkstemp('.json')
+    print filename
+    os.close(fd)
+    fptr = open(filename, 'w+b')
+    processed = 0
+    usemmap = False
+    for f in files:
+        starts = f['starts']
+        if usemmap:
+            fd = os.open(f['name'], os.O_RDONLY)
+            data = mmap.mmap(fd, 0, access=mmap.ACCESS_READ)
+        else:
+            data = open(f['name']).read()
+            os.unlink(f['name'])
+        pickList = range(len(starts)-1)
+        random.shuffle(pickList)
+        for pick in pickList:
+            fptr.write(data[starts[pick]:starts[pick+1]])
+            processed += 1
+            if not processed % 1000:
+                elapsed = time.time() - starttime
+                if elapsed:
+                    left = (numRows - processed) * (elapsed / processed)
+                else:
+                    left = 0
+                sys.stdout.write('%d/%d %3.1fs  %3.1fs left  \r' % (
+                    processed, numRows, elapsed, left))
+                sys.stdout.flush()
+        if usemmap:
+            data.close()
+            os.close(fd)
+            os.unlink(f['name'])
+            fd = None
+        data = None
+    fptr.close()
+    
+    client = getDbConnection(destDB)
+    database = client.get_default_database()
+    destColl = database['trips']
+    destColl.drop()
+    destColl.drop_indexes()
+    if not opts.get('dropIndex', False):
+        indexTrips(opts)
+    destColl = None
+
+    cmd = ('""c:\\Program Files\\MongoDB 2.6 Standard\\bin\\mongoimport.exe" ' 
+        '--db=%s --collection=trips --file="%s" --drop""' % (destDB, filename))
+    os.system(cmd)
+    os.unlink(filename)
+    
+    if opts.get('endIndex', False):
+        starttime = time.time()
+        sys.stderr.write('Indexing\n')
+        indexTrips(opts)
+        sys.stderr.write('Indexed %3.1fs\n' % (time.time() - starttime, ))
+
+
 def dehash(medallions=None, hacks=None):
     """
-    Create a hacks and a medallions collection in the mongo database at
-    mongodb://localhost:27017/taxi that can be used to deanonymize the hack and
-    medallion hashes.
+    Create tables that can be used to deanonymize the hack and medallion 
+    hashes.
 
-    :param medallions: a list of known medallions.  None to fetch it from the
-                       database.
-    :param hacks: a list of known hack licenses.  None to fetch it from the
-                  database.
+    :param medallions: a list of known medallions.
+    :param hacks: a list of known hack licenses.
     :return medallionTable: a dictionary with md5sum keys and dehashed values.
     :return hackTable: a dictionary with md5sum keys and dehashed values.
     """
-    client = None
-    if not medallions or not hacks:
-        client = getDbConnection()
-        database = client.get_default_database()
+    medallionTable = hackTable = None
 
-    if True:
+    if medallions:
         MainKey = getKey('medallion')
-        if client:
-            collection = database['medallions']
-            collection.drop()
-            medallions = list(database['trips'].find(
-                {}, fields=[MainKey]).distinct(MainKey))
-        print 'Medallions', len(medallions), medallions[:5]
+        print 'Medallions', len(medallions)
         medallions = dict.fromkeys(medallions)
         table = {}
         letstr = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -150,24 +261,13 @@ def dehash(medallions=None, hacks=None):
                         sys.stderr.write('%8s %d \r' % (
                             value, len(table)))
                         sys.stderr.flush()
-        for key in medallions:
-            if key not in table:
-                print 'Missing medallion', key
         print 'Found %d medallion hashes out of %d' % (len(table),
                                                        len(medallions))
-        if client:
-            for key in table:
-                collection.save({MainKey: key, 'value': table[key]})
         medallionTable = table
 
-    if True:
+    if hacks:
         MainKey = getKey('hack_license')
-        if client:
-            collection = database['hacks']
-            collection.drop()
-            hacks = list(database['trips'].find(
-                {}, fields=[MainKey]).distinct(MainKey))
-        print 'Hack Licenses', len(hacks), hacks[:5]
+        print 'Hack Licenses', len(hacks)
         hacks = dict.fromkeys(hacks)
         table = {}
         # https://medium.com/@vijayp/of-taxis-and-rainbows-f6bc289679a1 claims
@@ -184,31 +284,32 @@ def dehash(medallions=None, hacks=None):
                     table[key] = value
                     sys.stderr.write('%8s %d \r' % (value, len(table)))
                     sys.stderr.flush()
-                    if ((num>=1000000 and num<5000000) or num>=6000000 or
-                            value.startswith('0')):
-                        sys.stderr.write('\n')
         for key in hacks:
             if key not in table:
                 print 'Missing hack', key
         print 'Found %d hack hashes out of %d' % (len(table), len(hacks))
-        if client:
-            for key in table:
-                collection.save({MainKey: key, 'value': table[key]})
         hackTable = table
     return medallionTable, hackTable
 
 
-def getDbConnection():
+def getDbConnection(db=None):
     """
     Connect to local mongo database named 'taxi'
 
-    :returns: client: a pymongo client.
+    :param db: the name of the db to connect to.  If this starts with mongodb,
+               it is the full mongodb uri.  If None, use DBName.
+    :returns: a pymongo client.
     """
     clientOptions = {
         'connectTimeoutMS': 15000,
         # 'socketTimeoutMS': 60000,
     }
-    dbUri = 'mongodb://localhost:27017/' + CollectionName
+    if db is None:
+        dbUri = 'mongodb://localhost:27017/' + DBName
+    elif db.startswith('mongodb:'):
+        dbUri = db
+    else:
+        dbUri = 'mongodb://localhost:27017/' + db
     retryTime = 1
     while True:
         try:
@@ -229,10 +330,11 @@ def getKey(key):
     return KeyTable.get(key, key)
 
 
-def indexTrips(uniqueTrips=False):
+def indexTrips(opts={}, uniqueTrips=False):
     """
     Drop and then create indices on the trips collection.
 
+    :param opts: options dictionary.  See readFiles().
     :param uniqueTrips: True to enforce (medallion, hack_license,
                         pickup_datetime) tuple uniqueness.
     """
@@ -247,9 +349,10 @@ def indexTrips(uniqueTrips=False):
     collection.ensure_index([(getKey('medallion'), 1)], background=True)
     collection.ensure_index([(getKey('hack_license'), 1)], background=True)
     collection.ensure_index([(getKey('pickup_datetime'), 1)], background=True)
-    collection.ensure_index(
-        [(getKey('random'), 1), (getKey('pickup_datetime'), 1)],
-        background=True)
+    if opts.get('random', False):
+        collection.ensure_index(
+            [(getKey('random'), 1), (getKey('pickup_datetime'), 1)],
+            background=True)
 
 
 def readFiles(opts={}, uniqueTrips=False):
@@ -259,9 +362,16 @@ def readFiles(opts={}, uniqueTrips=False):
     collection of a mongo database at mongodb://localhost:27017/taxi.  Any
     existing trips collection is dropped.  The opts dictionary can contain the
     following items:
+        dateAsInt: if True, store dates as epoch millisecond integers
+        dropIndex: if True, do not create indices after dropping the collection
+    an indices.
+        endIndex: if True, create indices after loading data.
+        fromMonth: if present, do not drop the collection before loading data,
+    and load data starting at the 1-based month.
+        random: if True, add a random number to each realized row.
         sampleRate: if a number, only keep a statistical average of 1 row in
     this many.
-        random: if True, add a random number to each realized row.
+        toMonth: stop loading after the specified 1-based month (inclusive).
 
     :param opts: options dictionary.  See above.
     :param uniqueTrips: True to enforce (medallion, hack_license,
@@ -273,30 +383,37 @@ def readFiles(opts={}, uniqueTrips=False):
     collection = database['trips']
     if not 'fromMonth' in opts:
         collection.drop()
-        indexTrips()
+        collection.drop_indexes()
+        if not opts.get('dropIndex', False):
+            indexTrips(opts)
     dcount = rcount = 0
     usedkeys = {}
+    starttime = time.time()
+    medallions = {}
+    hacks = {}
     for month in xrange(opts.get('fromMonth', 1), opts.get('toMonth', 12)+1):
         data = zipfile.ZipFile('trip_data_%d.csv.zip' % month)
         data = csv.reader(data.open(data.namelist()[0]))
-        dheader = [getKey(key.strip()) for key in data.next()]
-        medallions = {}
-        hacks = {}
-        triples = {}
+        dheader = [key.strip() for key in data.next()]
+        columns = {dheader[col]: col for col in xrange(len(dheader))}
+        m_col = columns['medallion']
+        h_col = columns['hack_license']
         linecount = 0
         for tripline in data:
-            trip = dict(zip(dheader, [val.strip() for val in tripline]))
-            medallions[trip[getKey('medallion')]] = True
-            hacks[trip[getKey('hack_license')]] = True
+            medallions[tripline[m_col]] = True
+            hacks[tripline[h_col]] = True
             linecount += 1
             if not linecount % 10000:
-                sys.stderr.write('%d %d %d %d\r' % (
-                    month, linecount, len(medallions), len(hacks)))
+                sys.stderr.write('%d %d %d %d %3.1fs\r' % (
+                    month, linecount, len(medallions), len(hacks),
+                    time.time() - starttime))
                 sys.stderr.flush()
-        sys.stderr.write('%d %d %d %d\n' % (
-            month, linecount, len(medallions), len(hacks)))
+        sys.stderr.write('%d %d %d %d %3.1fs\n' % (
+            month, linecount, len(medallions), len(hacks),
+            time.time() - starttime))
         sys.stderr.flush()
-        medallionTable, hackTable = dehash(medallions.keys(), hacks.keys())
+    medallionTable, hackTable = dehash(medallions.keys(), hacks.keys())
+    for month in xrange(opts.get('fromMonth', 1), opts.get('toMonth', 12)+1):
         data = zipfile.ZipFile('trip_data_%d.csv.zip' % month)
         data = csv.reader(data.open(data.namelist()[0]))
         dheader = [getKey(key.strip()) for key in data.next()]
@@ -304,12 +421,9 @@ def readFiles(opts={}, uniqueTrips=False):
         fare = csv.reader(fare.open(fare.namelist()[0]))
         fheader = [getKey(key.strip()) for key in fare.next()]
         bulk = collection.initialize_unordered_bulk_op()
+        triples = {}
         for tripline in data:
             trip = dict(zip(dheader, [val.strip() for val in tripline]))
-            trip[getKey('medallion')] = medallionTable.get(
-                trip[getKey('medallion')], trip[getKey('medallion')])
-            trip[getKey('hack_license')] = hackTable.get(
-                trip[getKey('hack_license')], trip[getKey('hack_license')])
             tripkey = (trip[getKey('medallion')], trip[getKey('hack_license')],
                 trip[getKey('pickup_datetime')])
             if (trip[getKey('medallion')] in usedkeys and trip[getKey(
@@ -322,10 +436,6 @@ def readFiles(opts={}, uniqueTrips=False):
                 if not fareline:
                     break
                 fdat = dict(zip(fheader, [val.strip() for val in fareline]))
-                fdat[getKey('medallion')] = medallionTable.get(
-                    fdat[getKey('medallion')], fdat[getKey('medallion')])
-                fdat[getKey('hack_license')] = hackTable.get(
-                    fdat[getKey('hack_license')], fdat[getKey('hack_license')])
                 fdatkey = (fdat[getKey('medallion')], fdat[getKey(
                     'hack_license')], fdat[getKey('pickup_datetime')])
                 triples[fdatkey] = fdat
@@ -341,6 +451,10 @@ def readFiles(opts={}, uniqueTrips=False):
             if (opts.get('sampleRate', None) and
                     random.random() * opts['sampleRate'] >= 1):
                 continue
+            trip[getKey('medallion')] = medallionTable.get(
+                trip[getKey('medallion')], trip[getKey('medallion')])
+            trip[getKey('hack_license')] = hackTable.get(
+                trip[getKey('hack_license')], trip[getKey('hack_license')])
 
             for key in trip:
                 if trip[key] == '':
@@ -366,8 +480,9 @@ def readFiles(opts={}, uniqueTrips=False):
             bulk.insert(trip)
             dcount += 1
             if not dcount % 1000:
-                sys.stderr.write('%d %d %d %d \r' % (
-                    month, dcount, rcount, len(triples)))
+                sys.stderr.write('%d %d %d %d %3.1fs \r' % (
+                    month, dcount, rcount, len(triples),
+                    time.time() - starttime))
                 sys.stderr.flush()
                 bulk.execute()
                 # Reload the database connection
@@ -386,8 +501,15 @@ def readFiles(opts={}, uniqueTrips=False):
                 bulk = collection.initialize_unordered_bulk_op()
         bulk.execute()
         data = None
-    sys.stderr.write('-- %d %d --\n' % (dcount, rcount))
+        sys.stderr.write('%d %d %d %d %3.1fs \n' % (
+            month, dcount, rcount, len(triples), time.time() - starttime))
+    sys.stderr.write('-- %d %d -- %3.1fs \n' % (
+        dcount, rcount, time.time() - starttime))
     sys.stderr.flush()
+    if opts.get('endIndex', False):
+        sys.stderr.write('Indexing\n')
+        indexTrips(opts)
+        sys.stderr.write('Indexed %3.1fs\n' % (time.time() - starttime, ))
 
 
 if __name__ == '__main__':
@@ -396,13 +518,18 @@ if __name__ == '__main__':
     opts = {}
     seed = 0
     for arg in sys.argv[1:]:
-        if arg.startswith('--collection='):
-            CollectionName = arg.split('=', 1)[1]
-        elif arg == '--compact':
+        if arg == '--compact':
             KeyTable = CKeyTable
             RevTable = CRevTable
-        elif arg == '--dehash':
-            actions['dehash'] = True
+        elif arg.startswith('--copy='):
+            actions['copy'] = arg.split('=', 1)[1]
+        elif arg.startswith('--db='):
+            DBName = arg.split('=', 1)[1]
+        elif arg == '--dropindex':
+            opts['dropIndex'] = True
+        elif arg == '--endindex':
+            opts['dropIndex'] = True
+            opts['endIndex'] = True
         elif arg.startswith('--from='):
             opts['fromMonth'] = int(arg.split('=', 1)[1])
         elif arg == '--index':
@@ -424,15 +551,19 @@ if __name__ == '__main__':
     if help or not len(actions):
         print """Load taxi zip files into a mongo database.
 
-Syntax: load_taxi.py --read --dehash --index --collection=(table)
-                     --sample=(rate) --seed=(value) --random
-                     --from=(month) --to=(month) --intdate --compact
+Syntax: load_taxi.py --db=(database) --read --index --copy=(database)
+        --sample=(rate) --seed=(value) --random --dropindex --endindex
+        --from=(month) --to=(month) --intdate --compact
 
---collection specifies the collection (table) to use in mongo.  This defaults
-    to 'taxi'.
+--db specifies the database to use in mongo.  This defaults to 'taxi'.
 --compact uses extra-compact keys for the database.
---dehash creates collections called 'hacks' and 'medallions' which can be used
-    to deanonymize the hack and medallion hashes.
+--copy copies the --db database to the --copy database using a random order.
+  The destination database will be indexed as per the --dropindex or --endindex
+  options.  If --random is not included, any random value will be stripped from
+  the database rows.  If included, a value will be added in the order the rows
+  are copied.
+--dropindex to drop the indices and not recreate them.
+--endindex recreates the indices at the end of the read process.
 --from does not drop or reindex existing data, and loads from the specified
     1-based month number onward.
 --index regenerates indices on the 'trips' collection.
@@ -448,10 +579,10 @@ Syntax: load_taxi.py --read --dehash --index --collection=(table)
 --to stops loading after the specified 1-based month number (inclusive)."""
         sys.exit(0)
     if 'index' in actions:
-        indexTrips()
+        indexTrips(opts)
+    if seed != '':
+        random.seed(int(seed))
     if 'read' in actions:
-        if seed != '':
-            random.seed(int(seed))
         readFiles(opts)
-    if 'dehash' in actions:
-        dehash()
+    if 'copy' in actions:
+        copyDB(DBName, actions['copy'], opts)
